598eb21b9637e30711a0a2ac3f68669238798f5b
justkelvin <muia.muinde@s.karu.ac.ke>
2022-11-02 10:54:13 +0300

    add metadata

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Title: Lab Environment Setup
+# Description: Sets up updates, installs essential apps, set up node and juiceshop
+# Author: Kelvin <redacted@gmail.com>
+# Date: 2022-11-02
+# Version: 1.0.0
+
