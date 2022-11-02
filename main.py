9a70141fdcbaba1d9e9d5210cf314730ca1d5436
justkelvin <muia.muinde@s.karu.ac.ke>
2022-11-02 14:25:42 +0300

    add install update func

--- main.py
+++ main.py
@@ -6,3 +6,49 @@
 # Date: 2022-11-02
 # Version: 1.0.0
 
+# Imports
+import os
+import sys
+from shutil import which
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--update', help='Updates the system to latest release.')
+
+
+args = parser.parse_args()
+updates = args.update
+
+def python_check():
+	"""Check for python version"""
+	if sys.version_info.major < (3):
+		sys.stderr.write("Your need a higher python version to use the script!!\n")
+		sys.exit(1)
+
+def update():
+	"""Debian apt update"""
+	sys.stdout.write("Performing repo update. Confirm with password when asked.")
+	time.sleep(3)
+	os.system('sudo apt update')
+	sys.exit(0)
+
+def package_check():
+	program = ['node', 'git', 'docker', 'virtualbox']
+	not_installed = []
+	for i in program:
+		installed = which(i)
+		
+		if installed == None:
+			not_installed.append(i)
+	print(not_installed)
+	sys.stdout.write("This programs have not been detected in your system!! Installing\n")
+			# print(f"Installing {i}")
+			# os.system('sudo apt install {i}')
+
+
+if __name__ == '__main__':
+	python_check()
+	# update()
+	package_check()
+
